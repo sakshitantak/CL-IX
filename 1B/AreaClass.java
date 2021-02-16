6fea7689fe00d3e68f69286d31ba9bd1c70cb175
pikaduck <sakshi99tantak@gmail.com>
2021-02-16 11:52:10 +0530

    1B added

---
+++ 1B/AreaClass.java
@@ -0,0 +1,8 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;  
+ 
+public interface AreaClass extends Remote {  
+   double findArea(int code,int length) throws RemoteException;
+   double findArea(int code,int height, int width) throws RemoteException;
+
+} 
